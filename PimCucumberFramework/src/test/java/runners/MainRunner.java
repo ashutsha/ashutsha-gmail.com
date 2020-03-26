43075d3e9a153cf9b74c97742b96bcd85edfacd2
Ash <ashutsha@outlook.com>
2020-03-26 19:01:44 +0000

    # WARNING: head commit changed in the meantime
    ok

--- PimCucumberFramework/src/test/java/runners/MainRunner.java
+++ PimCucumberFramework/src/test/java/runners/MainRunner.java
@@ -1,7 +1,6 @@
 package runners;
 
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
@@ -9,8 +8,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = { "src/test/java/resources/features/" }, glue = {
-		"stepDefinitions" }, monochrome = true, tags = {}, dryRun = false, plugin = { "pretty", "html:target/cucumber",
-				"json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
+		"stepDefinitions" }, monochrome = true, tags = {}, dryRun=false,
+				plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
 
 public class MainRunner extends AbstractTestNGCucumberTests {
 }
