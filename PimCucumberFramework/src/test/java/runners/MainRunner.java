f177eb701ce4c5c40286ec932ca9cec978adad57
Ash <ashutsha@outlook.com>
2020-03-26 18:20:51 +0000

    ok

--- PimCucumberFramework/src/test/java/runners/MainRunner.java
+++ PimCucumberFramework/src/test/java/runners/MainRunner.java
@@ -1,6 +1,7 @@
 package runners;
 
 import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
@@ -8,9 +9,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = { "src/test/java/resources/features/" }, glue = {
-		"stepDefinitions" }, monochrome = true, tags = {}, 
-				plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
-				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
+		"stepDefinitions" }, monochrome = true, tags = {}, dryRun = false, plugin = { "pretty", "html:target/cucumber",
+				"json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
 
 public class MainRunner extends AbstractTestNGCucumberTests {
 }
